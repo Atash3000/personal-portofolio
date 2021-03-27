e1232520b0fbde2b64fffa81254fd7aae2bb48c1
Atamurad <Atamurad.babakulyyev@gmail.com>
2021-03-27 17:26:54 -0400

    stle workflow component

--- src/components/WorkFlow/projects.js
+++ src/components/WorkFlow/projects.js
@@ -10,16 +10,17 @@ export default  [
   {
     "id":2,
     "name":"Dating app",
-    "image": '/static/media/dating-app.14cfbb54.jpg',
-    "link":"www.google.com",
+    
+    "image":'/static/media/dating-app.14cfbb54.jpg',
+    "link":"https://safari-dubai.netlify.app/",
     "description":"Real Dating app to help people find a love"
 
   },
   {
     "id":3,
     "name":"Safari",
-    "image": '/static/media/dating-app.14cfbb54.jpg',
-    "link":"",
+    "image": '/static/media/safari.3c814a27.jpg',
+    "link":"https://safari-dubai.netlify.app/",
     "description":"Dubai Safari Tours"
 
   },
@@ -27,7 +28,7 @@ export default  [
     "id":4,
     "name":"Safari",
     "image": '/static/media/dating-app.14cfbb54.jpg',
-    "link":"",
+    "link":"https://safari-dubai.netlify.app/",
     "description":"Dubai Safari Tours"
 
   },
@@ -35,15 +36,15 @@ export default  [
     "id":5,
     "name":"Safari",
     "image": '/static/media/dating-app.14cfbb54.jpg',
-    "link":"",
+    "link":"https://safari-dubai.netlify.app/",
     "description":"Dubai Safari Tours"
 
   },
   {
     "id":6,
     "name":"Safari",
-    "image": '/static/media/dating-app.14cfbb54.jpg',
-    "link":"",
+    "image": '/static/media/safari.3c814a27.jpg',
+    "link":"https://safari-dubai.netlify.app/",
     "description":"Dubai Safari Tours"
 
   }
