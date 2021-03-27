4d43ec29123204ec773dcb0703442546e6eb6eb4
Atamurad <Atamurad.babakulyyev@gmail.com>
2021-03-26 21:30:01 -0400

    add home page slider

--- src/components/HomeSlider/HomeSlider.js
+++ src/components/HomeSlider/HomeSlider.js
@@ -9,7 +9,14 @@ function HomeSlider() {
   return (
     <div className='image-slider '>
       <div className="image-container">
-      
+        <div className="text-box">
+          <h1 className="heading">
+            <span className="heading--main">
+              hi i am a sofware engineer
+            </span>
+            <span className="heading--sub">a full stack</span>
+          </h1>
+        </div>
       </div>
       
     </div>
