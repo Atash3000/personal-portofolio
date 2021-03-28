0a0e8d43d4634450069caacc7aa642a6242fcc68
Atamurad <Atamurad.babakulyyev@gmail.com>
2021-03-28 09:11:14 -0400

    update workflow component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,12 @@
 import './Header.scss';
 import React from 'react';
 import Navigation from './Navigation';
-import {Route,Switch} from 'react-router-dom';
-import HomeSlider from '../HomeSlider/HomeSlider';
-import About from '../About/About';
-import WorkFlow from '../WorkFlow/WorkFlow';
+
 
 function Header() {
   return (
     <header className='header'>
       <Navigation/>
-      <Route exact path='/'> <HomeSlider/> </Route>
-    
-          <About />
-          <WorkFlow />
     </header>
   )
 }
--- src/components/WorkFlow/WorkFlow.js
+++ src/components/WorkFlow/WorkFlow.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import './WorkFlow.scss';
 import projects from './projects.js';
-import {Link} from 'react-router-dom';
-import hero from './images/dating-app.jpg'
+
+import datingApp from './images/dating-app.jpg';
+import safari from './images/safari.jpg';
+
 
 function WorkFlow() {
   return (
@@ -11,28 +13,88 @@ function WorkFlow() {
         <h2 className="workflow__text">the projects i recently worked on</h2>
       </div>
       <div className="grid-card">
-        {projects.map(project =>{
-          const {name,image,description,id,link} = project;
-          
-          return(
-            <div key={id} className="grid-card__items"> 
-              <div className="grid-card__image-box" >
-              <img  className="grid-card__image"  src={image} />
-              </div>
-              <div className="grid-card__description">
-              <h3 className="grid-card__name">{name}</h3>
-                <p className="grid-card__text">
-                  {description}
-                </p>
-              </div>
-
-              
-            
-              <a  target="_blank" href={link} className="grid-card__link" > learn more &#8594; </a>
-              
+        <div className="grid-card__items"> 
+          <div className="grid-card__image-box" >
+              <img  className="grid-card__image"  src={datingApp} />
             </div>
-          )
-        })}
+          <div className="grid-card__description">
+              <h3 className="grid-card__name">Dating app</h3>
+              <p className="grid-card__text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam ex at dicta.
+              </p>
+          </div>
+            <a  target="_blank" href='#' className="grid-card__link" > learn more &#8594; </a>
+          </div>
+          <div className="grid-card__items"> 
+          <div className="grid-card__image-box" >
+              <img  className="grid-card__image"  src={safari} />
+            </div>
+          <div className="grid-card__description">
+              <h3 className="grid-card__name">Dating app</h3>
+              <p className="grid-card__text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam ex at dicta.
+              </p>
+          </div>
+            <a  target="_blank" href='#' className="grid-card__link" > learn more &#8594; </a>
+          </div>
+
+          <div className="grid-card__items"> 
+          <div className="grid-card__image-box" >
+              <img  className="grid-card__image"  src={datingApp} />
+            </div>
+          <div className="grid-card__description">
+              <h3 className="grid-card__name">Dating app</h3>
+              <p className="grid-card__text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam ex at dicta.
+              </p>
+          </div>
+            <a  target="_blank" href='#' className="grid-card__link" > learn more &#8594; </a>
+          </div>
+
+
+          <div className="grid-card__items"> 
+          <div className="grid-card__image-box" >
+              <img  className="grid-card__image"  src={datingApp} />
+            </div>
+          <div className="grid-card__description">
+              <h3 className="grid-card__name">Dating app</h3>
+              <p className="grid-card__text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam ex at dicta.
+              </p>
+          </div>
+            <a  target="_blank" href='#' className="grid-card__link" > learn more &#8594; </a>
+          </div>
+
+
+          <div className="grid-card__items"> 
+          <div className="grid-card__image-box" >
+              <img  className="grid-card__image"  src={datingApp} />
+            </div>
+          <div className="grid-card__description">
+              <h3 className="grid-card__name">Dating app</h3>
+              <p className="grid-card__text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam ex at dicta.
+              </p>
+          </div>
+            <a  target="_blank" href='#' className="grid-card__link" > learn more &#8594; </a>
+          </div>
+
+
+          <div className="grid-card__items"> 
+          <div className="grid-card__image-box" >
+              <img  className="grid-card__image"  src={datingApp} />
+            </div>
+          <div className="grid-card__description">
+              <h3 className="grid-card__name">Dating app</h3>
+              <p className="grid-card__text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam ex at dicta.
+              </p>
+          </div>
+            <a  target="_blank" href='#' className="grid-card__link" > learn more &#8594; </a>
+          </div>
+
+
+          
       </div>
     </div>
   )
