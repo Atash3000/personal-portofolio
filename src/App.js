764ef2bdcc24f5d972ef53fc0cc9d8f3c82e9352
Atamurad <Atamurad.babakulyyev@gmail.com>
2021-03-28 15:12:21 -0400

    add hide menu function

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@
 import './App.scss';
 import Hamburger from './components/Hamburger/Hamburger';
 import Header from './components/Header/Header';
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 import MobileMenu from './components/MobileMenu/MobileMenu';
 import HomeSlider from './components/HomeSlider/HomeSlider';
 import MediaBar from './components/SocialMedia/MediaBar';
 import About from './components/About/About';
-import {Route,Switch} from 'react-router-dom';
+import {Route,Switch,Redirect} from 'react-router-dom';
 import WorkFlow from './components/WorkFlow/WorkFlow';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 
 
 function App() {
+
+
+  
+
   const [open ,setOpen] = useState('')
+
+  const hideMenu=()=>{
+    setOpen('')
+  }
   const showMenu =()=>{
     setOpen('open');
     
@@ -34,11 +42,12 @@ function App() {
       <Route exact path='/work-flow'> <WorkFlow /> </Route>
     
       </Switch>
+       
       {/* <Route  path='/'> <Footer /> </Route> */}
       <Footer/>
         <Hamburger showMenu={showMenu} open={open}/>
         <MediaBar/>
-        <MobileMenu/>
+        <MobileMenu hideMenu={hideMenu}/>
       </div>
       <main>
       </main>
--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,19 +3,19 @@ import './MobileMenu.scss';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
-function MobileMenu() {
+function MobileMenu({hideMenu}) {
   return (
     <div className="mobile-menu">
       <div className="mobile__inner">
         <nav className='mobile__nav'>
         <ul className='mobile__menu'>
-        <li className='mobile__items'><Link className='mobile__link' to="/">home</Link></li>
+        <li onClick={hideMenu} className='mobile__items'><Link className='mobile__link' to="/">home</Link></li>
 
-        <li className='mobile__items'><Link className='mobile__link' to="/workflow">work flow</Link></li>
+        <li  onClick={hideMenu} className='mobile__items'><Link className='mobile__link' to="/workflow">work flow</Link></li>
 
-        <li className='mobile__items'><Link className='mobile__link' to="/about">about me</Link></li>
+        <li  onClick={hideMenu} className='mobile__items'><Link className='mobile__link' to="/about">about me</Link></li>
 
-        <li className='mobile__items'><Link className='mobile__link' to="/contact">contact</Link></li>
+        <li  onClick={hideMenu} className='mobile__items'><Link className='mobile__link' to="/contact">contact</Link></li>
 
       </ul>
     </nav>      
