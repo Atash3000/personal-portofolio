6edd6f1fd7b2a12d1f3e8e1e1c61e6a0097aa4d2
Atamurad <Atamurad.babakulyyev@gmail.com>
2021-03-29 17:26:47 -0400

    add refirect component from react router dom

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
       <div className="wrapper">
       <div className={`App__container App__container--${open}`}>
       <Header/>
+      <Redirect to ='/'/>
       <Switch> 
      
       <Route exact path='/workflow' > <WorkFlow /> </Route> 
